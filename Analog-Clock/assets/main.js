5bca760bb7be22e2fd139f96079a8c158158b52e
YairBorn <yairb775@gmail.com>
2021-07-19 23:35:56 +0300

    project changes

--- Analog-Clock/assets/main.js
+++ Analog-Clock/assets/main.js
@@ -19,20 +19,102 @@ const clock = () => {
 
 setInterval(clock, 1000); // 1000 means 1s delay
 
-
 /********* CLOCK & DATE TEXT **********/
 
-const textHour = document.getElementById('text-hour'),
-    textMinutes = document.getElementById('text-minutes'),
-    textAMPM = document.getElementById('text-ampm'),
-    dateDay = document.getElementById('date-day'),
-    dateDay = document.getElementById('date-day'),
-    dateMonth = document.getElementById('date-month'),
-    dateYear = document.getElementById('date-year');
+const textHour = document.getElementById("text-hour"),
+  textMinutes = document.getElementById("text-minutes"),
+  textAMPM = document.getElementById("text-ampm"),
+  dateDay = document.getElementById("date-day"),
+  dateMonth = document.getElementById("date-month"),
+  dateYear = document.getElementById("date-year");
 
 const clockText = () => {
-    
+  let date = new Date();
+
+  let hh = date.getHours(),
+    ampm,
+    mm = date.getMinutes(),
+    day = date.getDate(),
+    month = date.getMonth(),
+    year = date.getFullYear();
+
+  if (hh >= 12) {
+    hh -= 12;
+    ampm = "PM";
+  } else {
+    ampm = "AM";
+  }
+
+  if (hh === 0) {
+    hh = 12;
+  }
+
+  if (hh < 10) {
+    hh = `0${hh}`;
+  }
+
+  if (mm < 10) {
+    mm = `0${mm}`;
+  }
+
+  textHour.innerHTML = `${hh}:`;
+  textMinutes.innerHTML = `${mm}`;
+  textAMPM.innerHTML = `${ampm}`;
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  dateDay.innerHTML = day;
+  dateMonth.innerHTML = `${months[month]}, `;
+  dateYear.innerHTML = `${year}`;
+};
+
+setInterval(clockText, 1000); // 1000 means 1s delay
+
+// ************************************ //
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bxs-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
-
-
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
